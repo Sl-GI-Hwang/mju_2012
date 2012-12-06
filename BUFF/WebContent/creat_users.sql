10dfa4785a89c21fa4409ba9b854e4718a0b5790
Sl-Gi-Hwang <h-sharks@hanmail.net>
2012-12-07 07:00:16 +0900

    BUFIX

---
+++ BUFF/WebContent/creat_users.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE picorhood DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+
+GRANT ALL ON picorhood.* TO 'web' IDENTIFIED BY 'project';
+
+use picorhood;
+
+
+CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY, 
+	userid VARCHAR(15) NOT NULL UNIQUE,
+	name VARCHAR(20),
+	pwd VARCHAR(20) NOT NULL, 
+	email VARCHAR(50) UNIQUE,
+	gender CHAR(1) NOT NULL
+);
+
+INSERT INTO users VALUES (1, 'Manager4', 'hyejung jang', '159357', 'hyejung-s@nate.com', 'F');
+
+
+CREATE TABLE picture (
+	pictureID INT AUTO_INCREMENT PRIMARY KEY, 
+	Date DATETIME,
+	Title VARCHAR(100) not null,
+	Text TEXT,
+	userid VARCHAR(15) NOT NULL,
+	FOREIGN KEY(userid) REFERENCES users(userid)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+);
+
+
